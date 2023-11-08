9efcf5b5ed56fa5203fdd0ba4443601bf6c265dc
Ricky0625 <rickywong0625@gmail.com>
2023-11-08 13:25:53 +0800

    chore: project init, hello ft_irc
    Hello ft_irc

---
+++ includes/irclib.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   irclib.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wricky-t <wricky-t@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 13:19:02 by wricky-t          #+#    #+#             */
+/*   Updated: 2023/11/08 13:23:40 by wricky-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IRCLIB_H
+# define IRCLIB_H
+
+# include <iostream>
+
+# include <sys/socket.h>
+# include <sys/types.h>
+# include <netdb.h>
+# include <arpa/inet.h>
+
+# include <signal.h>
+# include <unistd.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+# include <poll.h>
+
+#endif
